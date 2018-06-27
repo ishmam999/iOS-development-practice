150fe3f8c9dbcdc1575bda7176c2c08c69702d6c
Ishmam Islam <ishmam2050@gmail.com>
2018-06-27 09:28:29 +0600

    Calculator

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ishmam Islam on 27/6/18.
+//  Copyright © 2018 Ishmam Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var numberOnScreen:Double = 0;
+    var previousNumber:Double = 0;
+    var performingMath = false;
+    var operation = 0;
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton)
+    {
+        if performingMath == true
+        {
+            label.text = String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+            performingMath = true
+        }
+        else
+        {
+            label.text = label.text! + String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+        }
+        
+    }
+    
+    @IBAction func buttons(_ sender: UIButton)
+    {
+        if label.text != "" && sender.tag != 11 && sender.tag != 16
+        {
+            previousNumber = Double(label.text!)!
+            
+            if sender.tag == 12 //divide
+            {
+                label.text = "/";
+            }
+            else if sender.tag == 13 //mutiply
+            {
+                label.text = "*";
+            }
+            else if sender.tag == 14 //substract
+            {
+                label.text = "-";
+            }
+            else if sender.tag == 15 //addition
+            {
+                label.text = "+";
+            }
+            
+            operation = sender.tag
+            performingMath = true;
+        }
+        else if sender.tag == 16
+        {
+            if operation == 12
+            {
+                label.text = String(previousNumber / numberOnScreen)
+            }
+            else if operation == 13
+            {
+                label.text = String(previousNumber * numberOnScreen)
+            }
+            else if operation == 14
+            {
+                label.text = String(previousNumber - numberOnScreen)
+            }
+            else if operation == 15
+            {
+                label.text = String(previousNumber + numberOnScreen)
+            }
+        }
+        else if sender.tag == 11
+        {
+            label.text = ""
+            previousNumber = 0
+            numberOnScreen = 0
+            operation = 0
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
